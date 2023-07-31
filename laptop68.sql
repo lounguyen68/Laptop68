deb4d259b241838c19b8464e195678e967713f3d
lounguyen68 <lou9.works@gmail.com>
2023-07-31 13:16:32 +0700

    create models

---
+++ laptop68.sql
@@ -0,0 +1,195 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Máy chủ: localhost
+-- Thời gian đã tạo: Th7 29, 2023 lúc 12:37 AM
+-- Phiên bản máy phục vụ: 8.0.17
+-- Phiên bản PHP: 7.3.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Cơ sở dữ liệu: `Laptop68`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `admins`
+--
+
+CREATE TABLE `admins` (
+  `id` int(11) NOT NULL,
+  `adminname` varchar(255) DEFAULT NULL COMMENT 'Tên đăng nhập',
+  `role` tinyint(1) NOT NULL DEFAULT 0,
+  `password` varchar(255) DEFAULT NULL COMMENT 'Mật khẩu đăng nhập',
+  `first_name` varchar(255) DEFAULT NULL COMMENT 'Fist name',
+  `last_name` varchar(255) DEFAULT NULL COMMENT 'Last name',
+  `phone` varchar(11) DEFAULT NULL COMMENT 'SĐT user',
+  `address` varchar(255) DEFAULT NULL COMMENT 'Địa chỉ user',
+  `email` varchar(255) DEFAULT NULL COMMENT 'Email của user',
+  `avatar` varchar(255) DEFAULT NULL COMMENT 'File ảnh đại diện',
+  `last_login` datetime DEFAULT NULL COMMENT 'Lần đăng nhập gần đây nhất',
+  `status` tinyint(3) DEFAULT 0 COMMENT 'Trạng thái danh mục: 0 - Inactive, 1 - Active',
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp() COMMENT 'Ngày tạo',
+  `updated_at` datetime DEFAULT NULL COMMENT 'Ngày cập nhật cuối'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `categories`
+--
+
+CREATE TABLE `categories` (
+  `id` int(11) NOT NULL,
+  `admin_id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL COMMENT 'Tên danh mục',
+  `type` tinyint(3) DEFAULT 0 COMMENT 'Loại danh mục: 0 - Product, 1 - News',
+  `avatar` varchar(255) DEFAULT NULL COMMENT 'Tên file ảnh danh mục',
+  `description` text DEFAULT NULL COMMENT 'Mô tả chi tiết cho danh mục',
+  `status` tinyint(3) DEFAULT 0 COMMENT 'Trạng thái danh mục: 0 - Inactive, 1 - Active',
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp() COMMENT 'Ngày tạo danh mục',
+  `updated_at` datetime DEFAULT NULL COMMENT 'Ngày cập nhật cuối'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `event`
+--
+
+CREATE TABLE `event` (
+  `id` int(11) NOT NULL,
+  `admin_id` int(11) NOT NULL,
+  `category_id` int(11) DEFAULT NULL,
+  `image_event` text DEFAULT NULL,
+  `description` text DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `news`
+--
+
+CREATE TABLE `news` (
+  `id` int(11) NOT NULL,
+  `admin_id` int(11) NOT NULL,
+  `category_id` int(11) DEFAULT NULL COMMENT 'Id của danh mục mà tin tức thuộc về, là khóa ngoại liên kết với bảng categories',
+  `name` varchar(255) NOT NULL COMMENT 'Tiêu đề tin tức',
+  `summary` text DEFAULT NULL COMMENT 'Mô tả ngắn cho tin tức',
+  `avatar` varchar(255) DEFAULT NULL COMMENT 'Tên file ảnh tin tức',
+  `content` text DEFAULT NULL COMMENT 'Mô tả chi tiết cho sản phẩm',
+  `status` tinyint(3) DEFAULT 0 COMMENT 'Trạng thái danh mục: 0 - Inactive, 1 - Active',
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp() COMMENT 'Ngày tạo',
+  `updated_at` datetime DEFAULT NULL COMMENT 'Ngày cập nhật cuối'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `orders`
+--
+
+CREATE TABLE `orders` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) DEFAULT NULL COMMENT 'Id của user trong trường hợp đã login và đặt hàng, là khóa ngoại liên kết với bảng users',
+  `fullname` varchar(255) DEFAULT NULL COMMENT 'Tên khách hàng',
+  `address` varchar(255) DEFAULT NULL COMMENT 'Địa chỉ khách hàng',
+  `mobile` varchar(11) DEFAULT NULL COMMENT 'SĐT khách hàng',
+  `email` varchar(255) DEFAULT NULL COMMENT 'Email khách hàng',
+  `note` text DEFAULT NULL COMMENT 'Ghi chú từ khách hàng',
+  `price_total` int(255) DEFAULT NULL COMMENT 'Tổng giá trị đơn hàng',
+  `payment_status` tinyint(2) DEFAULT 0 COMMENT 'Trạng thái đơn hàng: 0 - Chưa thành toán, 1 - Đã thành toán',
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp() COMMENT 'Ngày tạo đơn',
+  `updated_at` datetime DEFAULT NULL COMMENT 'Ngày cập nhật cuối'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `order_details`
+--
+
+CREATE TABLE `order_details` (
+  `order_id` int(11) DEFAULT NULL COMMENT 'Id của order tương ứng, là khóa ngoại liên kết với bảng orders',
+  `product_id` int(11) NOT NULL COMMENT 'ID sp tại thời điểm đặt hàng',
+  `product_price` int(11) DEFAULT NULL COMMENT 'Giá sản phẩm tương ứng tại thời điểm đặt hàng',
+  `quantity` int(11) DEFAULT NULL COMMENT 'Số lượng sản phẩm tương ứng tại thời điểm đặt hàng'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+
+-- --------------------------------------------------------
+
+--
+-- Bẫy `order_details`
+--
+DELIMITER $$
+CREATE TRIGGER `create_orders` AFTER INSERT ON `order_details` FOR EACH ROW BEGIN
+    DECLARE quantity INT;
+    DECLARE product_id INT;
+    SELECT NEW.quantity, NEW.product_id INTO quantity, product_id;
+    
+    IF (SELECT amount FROM products WHERE id = product_id) >= quantity THEN
+        UPDATE products SET amount = amount - quantity WHERE id = product_id;
+    ELSE
+        SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'Not enough products in stock.';
+    END IF;
+END
+$$
+DELIMITER ;
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `products`
+--
+
+CREATE TABLE `products` (
+  `id` int(11) NOT NULL,
+  `admin_id` int(11) NOT NULL,
+  `category_id` int(11) DEFAULT NULL COMMENT 'Id của danh mục mà sản phẩm thuộc về, là khóa ngoại liên kết với bảng categories',
+  `title` varchar(255) DEFAULT NULL COMMENT 'Tên sản phẩm',
+  `avatar` text DEFAULT NULL COMMENT 'Tên file ảnh sản phẩm',
+  `color` varchar(55) DEFAULT NULL COMMENT 'Màu sắc',
+  `price` int(11) DEFAULT NULL COMMENT 'Giá sản phẩm',
+  `amount` int(11) DEFAULT NULL COMMENT 'Số lượng sản phẩm trong kho',
+  `summary` varchar(255) DEFAULT NULL COMMENT 'Mô tả ngắn cho sản phẩm',
+  `content` text DEFAULT NULL COMMENT 'Mô tả chi tiết cho sản phẩm',
+  `status` tinyint(3) DEFAULT 1 COMMENT 'Trạng thái danh mục: 0 - Inactive, 1 - Active',
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp() COMMENT 'Ngày tạo',
+  `updated_at` datetime DEFAULT NULL COMMENT 'Ngày cập nhật cuối'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `username` varchar(255) DEFAULT NULL COMMENT 'Tên đăng nhập',
+  `password` varchar(255) DEFAULT NULL COMMENT 'Mật khẩu đăng nhập',
+  `first_name` varchar(255) DEFAULT NULL COMMENT 'Fist name',
+  `last_name` varchar(255) DEFAULT NULL COMMENT 'Last name',
+  `phone` varchar(11) DEFAULT NULL COMMENT 'SĐT user',
+  `address` varchar(255) DEFAULT NULL COMMENT 'Địa chỉ user',
+  `email` varchar(255) DEFAULT NULL COMMENT 'Email của user',
+  `avatar` varchar(255) DEFAULT NULL COMMENT 'File ảnh đại diện',
+  `last_login` datetime DEFAULT NULL COMMENT 'Lần đăng nhập gần đây nhất',
+  `facebook` varchar(255) DEFAULT NULL COMMENT 'Link facebook',
+  `status` tinyint(3) DEFAULT 0 COMMENT 'Trạng thái danh mục: 0 - Inactive, 1 - Active',
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp() COMMENT 'Ngày tạo',
+  `updated_at` datetime DEFAULT NULL COMMENT 'Ngày cập nhật cuối'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
